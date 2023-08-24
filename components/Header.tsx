f06b7625806bd4ad3cf539878a86a45585310653
Oleksandr FIlippov <110115726+AlexFilp@users.noreply.github.com>
2023-08-24 22:53:08 +0200

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-center items-center gap-[17px] p-[20px] bg-black">
+    <header className="flex justify-center items-center gap-[17px] p-[20px] bg-black relative">
       <Link href="/" className="link">
         Home
       </Link>
@@ -12,6 +12,12 @@ export default function Header() {
       <Link href="/blog" className="link">
         Blog
       </Link>
+      <button
+        type="button"
+        className="text-white border border-white rounded px-[7px] py-[5px] absolute top-[50%] translate-y-[-50%] right-[25px] hover:scale-105 transition-all ease-linear duration-200ms hover:border-red-600 hover:text-red-600"
+      >
+        Change theme
+      </button>
     </header>
   );
 }
