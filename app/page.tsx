d97c0fedc7cb730ade8a49799e4180b8a35968f4
Oleksandr FIlippov <110115726+AlexFilp@users.noreply.github.com>
2023-08-24 20:07:47 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function HomePage() {
-  return <h1 className="text-[50px]">Home page</h1>;
+  return <h1 className="title">Home page</h1>;
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer>Created by ALex Filp</footer>;
+}
