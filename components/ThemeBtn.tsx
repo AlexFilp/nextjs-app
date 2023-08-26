09794707f59dc752c3c673aead5cb9c4838430b0
Oleksandr FIlippov <110115726+AlexFilp@users.noreply.github.com>
2023-08-26 11:46:15 +0200

    Update ThemeBtn.tsx

--- components/ThemeBtn.tsx
+++ components/ThemeBtn.tsx
@@ -1,28 +1,33 @@
 "use client";
-import { useLayoutEffect, useState } from "react";
-export const ThemeBtn = () => {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
+import { useState, useEffect } from "react";
 
-  useLayoutEffect(() => {
-    if (theme !== "light") {
+export const ThemeBtn = () => {
+  const [darkMode, setDarkMode] = useState(
+    window.localStorage.getItem("darkMode") ?? "false"
+  );
+
+  useEffect(() => {
+    if (darkMode === "false") {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.remove("dark");
     }
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-  const handleSwitchTheme = () => {
-    console.log(localStorage.theme);
-    if (theme === "light") {
-      setTheme("dark");
+    window.localStorage.setItem("darkMode", darkMode);
+  }, [darkMode]);
+
+  const handleDarkMode = () => {
+    console.log(window.localStorage.darkMode);
+    console.log(darkMode);
+    if (darkMode === "false") {
+      setDarkMode("true");
     } else {
-      setTheme("light");
+      setDarkMode("false");
     }
   };
 
   return (
     <button
-      onClick={handleSwitchTheme}
+      onClick={handleDarkMode}
       type="button"
       className="text-white border border-white rounded px-[7px] py-[5px] absolute top-[50%] translate-y-[-50%] right-[25px] hover:scale-105 transition-all ease-linear duration-200ms hover:border-red-600 hover:text-red-600"
     >
